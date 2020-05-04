9674467d9d50096bb0b229054513c1d3d7e76502
MkItSimple <choanania1@gmail.com>
2020-05-04 10:11:43 +0800

    p1-supportFragmentManager-1

--- app/src/main/java/com/example/ktfragmentpractice/MainActivity.kt
+++ app/src/main/java/com/example/ktfragmentpractice/MainActivity.kt
@@ -2,13 +2,44 @@ package com.example.ktfragmentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
+//https://www.youtube.com/watch?v=-vAI7RSPxOA
+//p1
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    //p1
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
 }
+
+
+/*
+* tutorial keypoints
+* 15:35 backstack
+* */
---
+++ app/src/main/java/com/example/ktfragmentpractice/SecondFragment.kt
@@ -0,0 +1,8 @@
+package com.example.ktfragmentpractice
+
+
+import androidx.fragment.app.Fragment
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+}
