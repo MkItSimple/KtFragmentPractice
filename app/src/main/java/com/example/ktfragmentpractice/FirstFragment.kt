325e1779e465595b1061afb198a47dae039fb45b
MkItSimple <choanania1@gmail.com>
2020-05-04 10:47:12 +0800

    p2-supportFragmentManager-2 CodingWithMitch Espresso

---
+++ app/src/main/java/com/example/ktfragmentpractice/FirstFragment.kt
@@ -0,0 +1,9 @@
+package com.example.ktfragmentpractice
+
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+
+}
--- app/src/main/java/com/example/ktfragmentpractice/MainActivity.kt
+++ app/src/main/java/com/example/ktfragmentpractice/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    private fun init() {
+        val bundle = Bundle()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, FirstFragment::class.java, bundle)
+            .commit()
     }
 }
